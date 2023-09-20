9c90f4ffd36d5d13ee1885c7e4069c6cd667c26e
Nihal8Gupta <139010543+Nihal8Gupta@users.noreply.github.com>
2023-09-20 12:01:59 +0530

    Create ugly.py

---
+++ ugly.py
@@ -0,0 +1,16 @@
+#WAP to check given number is ugly or not
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n == 0:
+            return False
+        res=[2, 3, 5]
+        while n!= 1:
+            for i in res:
+               if n%i==0:
+                   n=n//i
+                   break
+            else:
+                return False
+        return True
+obj=Solution()
+obj.isUgly(8)
